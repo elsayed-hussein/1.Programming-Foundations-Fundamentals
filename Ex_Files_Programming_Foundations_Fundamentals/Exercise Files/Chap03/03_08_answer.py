d7c17fca2b0c28159f462ba428ebcd91a71a1f6c
elsayed-hussen <elsayed.hussen@gmail.com>
2021-09-01 14:40:11 +0200

    03_08_answer

---
+++ Ex_Files_Programming_Foundations_Fundamentals/Exercise Files/Chap03/03_08_answer.py
@@ -0,0 +1,12 @@
+# A message for the user
+message = "This is going to be tricky ;)"
+Message = "Very tricky!"
+print(message)#show the message on the screen
+
+#Perform mathematical operations
+result = 2**3
+print("2**3 =",result)
+result = 5-3
+#print("5-3 =", result)
+
+print("Challenge Complete!")
