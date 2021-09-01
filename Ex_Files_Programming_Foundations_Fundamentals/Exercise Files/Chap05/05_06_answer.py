5a359adeed240550bf2f944d1023e688fed5f417
elsayed-hussen <elsayed.hussen@gmail.com>
2021-09-01 19:58:38 +0200

    05_06_answer

---
+++ Ex_Files_Programming_Foundations_Fundamentals/Exercise Files/Chap05/05_06_answer.py
@@ -0,0 +1,7 @@
+# Prints out the name of a favorite city
+def favorite_city(name):
+    print("One of my favorite cities is", name)
+
+favorite_city("cairo")
+favorite_city("abuammahd")
+favorite_city("ismailia")
